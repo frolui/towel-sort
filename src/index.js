db7cf27048bce78892b1f4f2578b620e0d7d875e
frolui <frolui29@gmail.com>
2021-02-11 21:54:04 +0300

    feat:add new functions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined) { return []; }
+
+  let towel = [];
+
+    function deeper(matrix) {
+        matrix.forEach((element ,i)=> {
+            
+            if (Array.isArray(element)) {
+                if (i % 2 !== 0) {
+                  element = element.reverse();}
+                  deeper(element);    
+            } else {
+                towel.push(+element)
+            }
+            
+        });
+    }
+
+    deeper(matrix)
+
+    return towel;
 }
